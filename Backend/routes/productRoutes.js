2ebaafa78acfa5363c579ee7ad06b1d25168300b
Lakshminarayana <lakshminarayanakatikala@gmail.com>
2025-10-24 22:15:56 +0530

    CRUD on product

--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -1,16 +1,18 @@
 const express = require('express')
-const {getProducts , addProduct } = require("../controllers/productController.js")
+const {getProducts , addProduct , deleteProduct , updateProduct , toggleStock} = require("../controllers/productController.js")
 const authMiddleware = require('../middleware/authMiddleware.js')
 
 const productRoute = express.Router()
 
 
 productRoute.get("/", getProducts)  // after complete the testing add authMiddleware for seurity
-productRoute.post("/add" ,addProduct)
+productRoute.post("/add", authMiddleware, addProduct);
 // productRoute.post("/upload" ,);
-// productRoute.delete("/:id",);
-// productRoute.put("/:id",);
-// productRoute.patch("/stock/:id",);
+productRoute.delete("/:id", authMiddleware ,deleteProduct );
+productRoute.put("/:id",authMiddleware , updateProduct);
+productRoute.patch("/stock/:id",authMiddleware , toggleStock);
 
 module.exports = productRoute
+
+//orders vastai 
 
