ea6946d153c00197292ef566dcf8f6995dc3a110
Lakshmi Narayana <lakshminarayanakatikala@gmail.com>
2025-11-09 11:21:04 +0530

    add user address

--- Backend/models/UserModel.js
+++ Backend/models/UserModel.js
@@ -18,4 +18,15 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 userSchema.index({ location: "2dsphere" });
+
+userSchema.virtual("addresses", {
+  ref: "Address",
+  localField: "_id",
+  foreignField: "userId",
+  justOne: false,
+});
+
+// Ensure virtuals are included in JSON responses
+userSchema.set("toObject", { virtuals: true });
+userSchema.set("toJSON", { virtuals: true });
 module.exports = mongoose.model("User", userSchema);
